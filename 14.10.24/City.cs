84122c9881ad9056d5d2e6fbc232a2d6d3061d36
Vova <lololoskafry@gmail.com>
2024-11-25 19:40:10 +0200

    123

--- 14.10.24/City.cs
+++ 14.10.24/City.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _14._10._24
 {
     internal class City
     {
+    
+        #region Areas
         private string[] areas;
         private string name;
         private string numb;
         private string country;
         private int population;
+        #endregion
+        #region Constructors
         public City() { }
         public City(string[] a, string n, string num, string c, int p) 
         {
@@ -22,7 +27,8 @@ namespace _14._10._24
             country = c;    
             population = p;
         }
-
+        #endregion
+        
         public void Show()
         {
             Console.WriteLine("Name: "+name);
@@ -35,6 +41,52 @@ namespace _14._10._24
                 Console.WriteLine(areas[i]);
             }
         }
+
+        #region OperationsOverrides
+        public static City operator +(City obj, int addPeople)
+        {
+            return new City(obj.areas, obj.name, obj.numb, obj.country, obj.population + addPeople);
+        }
+
+        public static City operator -(City obj, int removePeople)
+        {
+            return new City(obj.areas, obj.name, obj.numb, obj.country, obj.population - removePeople);
+        }
+        public static bool operator <(City a, City b)
+        {
+            if (a.population < b.population)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator >(City a, City b)
+        {
+            if (a.population > b.population)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator ==(City a, City b)
+        {
+            if (a.population == b.population)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator !=(City a, City b)
+        {
+            if (a.population == b.population)
+                return true;
+            else
+                return false;
+        }
+
+        #endregion
+
+        #region GetSet
         public void SetArea(string[] a) { areas = a; }
         public void SetName(string n) { name = n; }
         public void SetNumb(string n) { numb = n; }
@@ -45,5 +97,6 @@ namespace _14._10._24
         public string GetNumb() { return numb; } 
         public string GetCountry() { return country; }
         public int GetPopulation() { return population; }
+        #endregion
     }
 }
--- 14.10.24/Employee.cs
+++ 14.10.24/Employee.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,15 +10,19 @@ namespace _14._10._24
 {
     internal class Employee
     {
+        #region Areas
         private string name;
         private DateTime birthdate;
         private string tel;
         private string email;
         private string position;
         private string[] resp;
+        private int Salary { get; set; }
+        #endregion
 
+        #region MethodsConst
         public Employee() { }
-       public Employee(string name, DateTime birthdate, string tel, string email, string position, string[] resp)
+       public Employee(string name, DateTime birthdate, string tel, string email, string position, string[] resp, int s)
         {
             this.name = name;
             this.birthdate = birthdate;
@@ -25,6 +30,7 @@ namespace _14._10._24
             this.email = email;
             this.position = position;
             this.resp = resp;
+            Salary = s;
         }
         public void Show()
         {
@@ -39,6 +45,41 @@ namespace _14._10._24
                 Console.WriteLine(resp[i]);
             }
         }
+        #endregion
+
+        #region Overrides
+        public static Employee operator +(Employee obj, int addS)
+        {
+            return new Employee(obj.name, obj.birthdate, obj.tel, obj.email, obj.position, obj.resp, obj.Salary + addS);
+        }
+
+        public static Employee operator -(Employee obj, int minS)
+        {
+            return new Employee(obj.name, obj.birthdate, obj.tel, obj.email, obj.position, obj.resp, obj.Salary - minS);
+        }
+
+        public static bool operator <(Employee a, Employee b)
+        {
+            return a.Salary < b.Salary;
+        }
+
+        public static bool operator >(Employee a, Employee b)
+        {
+            return a.Salary > b.Salary;
+        }
+
+        public static bool operator ==(Employee a, Employee b)
+        {
+            return a.Salary == b.Salary;
+        }
+
+        public static bool operator !=(Employee a, Employee b)
+        {
+            return a.Salary != b.Salary;
+        }
+        #endregion
+
+        #region GetSet
         public string GetName() { return name; }
         public DateTime GetBirthdate() { return birthdate; }
         public string GetTel() { return tel; } 
@@ -50,5 +91,6 @@ namespace _14._10._24
         public void SetTel(string tel) { this.tel = tel; }
         public void SetEmail(string email) { this.email = email; }
         public void SetPosition(string position) { this.position = position; }
+        #endregion
     }
 }
